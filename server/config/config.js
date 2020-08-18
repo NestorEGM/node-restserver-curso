11738b993c5ab78d972089871dbfb6cffb208472
Nestor <nestor_gm02@hotmail.com>
2020-08-18 17:05:02 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://NestorEGM:9jYkJszj4fXDpLT9@cluster0.hssaa.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDB;
